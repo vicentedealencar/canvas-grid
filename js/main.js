6c346f8f08f51bc6827b71164066e5f29d598154
Vicente de Alencar <vicentedealencar@gmail.com>
2012-11-15 03:41:46 -0200

    blinking shit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 require(["canvas-grid"], function (canvasWatch) {
-	window.grid = [
-	    ["#FF0000", "blue", "rgb(0,255,0)"],
-	    ["#333", "#888", "#ddd"],
-	    ["#333", "#000", "#ddd"],
-	    ["#333", "#888", "#700"],
+    grid = [
+	    ["black", "black", "black"],
+	    ["black", "black", "black"],
+	    ["black", "black", "black"],
 	];
 	canvasWatch("myCanvas", grid);
+
+	setInterval(function () {
+	    for (var i = 0; i < grid.length; i++) {
+	        for (var j = 0; j < grid[i].length; j++) {
+	            grid[i][j] = "black";
+	        }
+	    }
+	    var x = ((Math.random() * 10) % grid.length).toFixed();
+	    var y = ((Math.random() * 10) % grid[x].length).toFixed();
+	    grid[x][y] = "red";
+	},1000);
 });
